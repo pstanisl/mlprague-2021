d319a21335193c4adc16b6702e6b682dd2f6cf32
Petr Stanislav <petr.stanislav@gmail.com>
2021-02-16 21:56:02 +0100

    initial util.py
    `load_movielens_data` for loading MovieLens data set

---
+++ utils.py
@@ -0,0 +1,16 @@
+import csv
+
+MOVIELENS_NUM_USERS = 943
+MOVIELENS_NUM_MOVIES = 1682
+
+def load_movielens_data(data_file):
+  """Loads the movielens data and returns the ratings matrix."""
+  ratings_matrix = np.zeros([MOVIELENS_NUM_USERS, MOVIELENS_NUM_MOVIES])
+  with open(data_file, 'r') as infile:
+    # The file is a csv with rows containing:
+    # user id | item id | rating | timestamp
+    reader = csv.reader(infile, delimiter='\t')
+    for row in reader:
+      user_id, item_id, rating, _ = row
+      ratings_matrix[int(user_id) - 1, int(item_id) - 1] = float(rating)
+  return ratings_matrix
